b2704bc4f3eaa46c5e0cdc4db4d614b6517f2dd8
nguyên_dev <leekhacnguyen156@gmail.com>
2021-10-18 11:19:19 +0700

    init

---
+++ app/src/main/java/com/example/ghichu_java2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ghichu_java2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+    }
+}
