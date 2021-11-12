87744116f4791a57660811d2e8869dbb6c6b1fac
nguyên_dev <leekhacnguyen156@gmail.com>
2021-11-12 20:30:18 +0700

    hi

--- app/src/main/java/com/example/ghichu_java2/MainActivity.java
+++ app/src/main/java/com/example/ghichu_java2/MainActivity.java
@@ -1,19 +1,49 @@
 package com.example.ghichu_java2;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference dr = database.getReference();
+//    DatabaseReference drload = database.getReference("sinhvien");
+//    private Date currentTime = Calendar.getInstance().getTime();
+//    private SimpleDateFormat sfd = new SimpleDateFormat("dd-MM-yyyy");
+//    private String date = sfd.format(currentTime);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+
+
+
     }
 }
